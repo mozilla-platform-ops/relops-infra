170552072ed3947d3a462048d95eb49d1dc47e03
Dave House <dhouse@mozilla.com>
2018-05-25 12:51:24 -0600

    list tc last job state | quarantine for each worker

--- moonshot/check_taskcluster_state.sh
+++ moonshot/check_taskcluster_state.sh
@@ -22,7 +22,17 @@ for c in {1..14}; do
         if ! (( c % 7 )) && (( i > 9 )); then
             break
         fi
+
         hostname=${hostname_prefix}$(printf "%03g" "${I}")
+        dc_name=mdc${dc}
+       taskId=$(wget -q -O - https://queue.taskcluster.net/v1/provisioners/releng-hardware/worker-types/${workerType}/workers/${dc_name}/${hostname} | grep 'taskId\|quarantine' | tail -1 | sed -e 's/quarantineUntil["\": ]*/ " "'${hostname}' quarantined /' | cut -d'"' -f4)
+        {
+       wget -q -O - https://queue.taskcluster.net/v1/task/${taskId}/status \
+            | grep 'workerId\|started\|"state"' | tr '\n' ' ' \
+            | sed -e 's/\"\([^ ]*\)\"[:]\?/ \1 /g' -e 's/state.*state//' | awk '{print $4" "$1" "$7}'
+        } \
+            | grep -o "${hostname}[^\",]*" || echo $taskId;
+false && {
         url="https://queue.taskcluster.net/v1/provisioners/releng-hardware/worker-types/${workerType}/workers/mdc${dc}/${hostname}"
         wget -q -O - "${url}" >/dev/null 2>&1 \
             || {
