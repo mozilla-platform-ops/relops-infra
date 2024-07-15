86c38f0f961e761f0219841a55efd73dfa7fcd4b
Andrew Erickson <aerickson@mozilla.com>
2024-07-15 11:06:49 -0700

    add todo

--- ci-configuration-tools/generate.sh
+++ ci-configuration-tools/generate.sh
@@ -6,6 +6,9 @@ export TASKCLUSTER_ROOT_URL="https://firefox-ci-tc.services.mozilla.com/"
 
 . ./venv/bin/activate
 
+# TODO: also requires `pip install -e .`
+#   - do it or warn
+#   - file issue to move to poetry?
 
 #ci-admin check --environment firefoxci
 ci-admin generate --json --environment firefoxci > generated.json
