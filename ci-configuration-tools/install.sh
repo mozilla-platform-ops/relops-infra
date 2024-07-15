b7567e6dc0dd7b02c94f091391a986ca3eff7b47
Andrew Erickson <aerickson@mozilla.com>
2024-07-15 10:48:02 -0700

    install: point at new git path

--- ci-configuration-tools/install.sh
+++ ci-configuration-tools/install.sh
@@ -5,4 +5,5 @@ set -x
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 
-./make_symlinks_for_executables.py --ignore "${SCRIPT_PATH}" /Users/aerickson/hg/ci-configuration
+#./make_symlinks_for_executables.py --ignore "${SCRIPT_PATH}" /Users/aerickson/hg/ci-configuration
+./make_symlinks_for_executables.py --ignore "${SCRIPT_PATH}" /Users/aerickson/git/fxci-config
