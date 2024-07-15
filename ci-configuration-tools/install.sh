fce561e7356c24ff6be1c221021a9f36f63a3b69
Andrew Erickson <aerickson@mozilla.com>
2024-07-15 14:08:48 -0700

    add todo

--- ci-configuration-tools/install.sh
+++ ci-configuration-tools/install.sh
@@ -5,5 +5,10 @@ set -x
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 
+# TODO: cleanup install dir with:
+#   gfind . -xtype l -delete
+#
+#   - otherwise create directory with all isntalled symlinks and remove those in target dir before installation every time
+
 #./make_symlinks_for_executables.py --ignore "${SCRIPT_PATH}" /Users/aerickson/hg/ci-configuration
 ./make_symlinks_for_executables.py --ignore "${SCRIPT_PATH}" /Users/aerickson/git/fxci-config
