9f44fbd5d91d88ddbd099c1ac981b32defab7a42
Ryan Curran <rcurran@mozilla.com>
2024-10-24 12:58:05 -0400

    quotes fix

--- moonshot/ronin_override_generator/ronin_override_generator.py
+++ moonshot/ronin_override_generator/ronin_override_generator.py
@@ -5,6 +5,11 @@ import os
 hostname = input("Enter the hostname to SSH into: ")
 username = input("Enter your SSH username: ")
 
+# Ensure username is provided
+if not username:
+    print("SSH username is required.")
+    exit(1)
+
 # Specify the path to the SSH private key file (defaulting to ~/.ssh/id_ed25519)
 ssh_key_path = input("Enter the path to your SSH private key file (or press enter to use '~/.ssh/id_ed25519'): ") or os.path.expanduser("~/.ssh/id_ed25519")
 
@@ -32,7 +37,7 @@ puppet_branch = input("Enter the puppet branch name: ")
 puppet_mail = input("Enter the puppet mail address: ")
 
 # Command to create the /etc/puppet/ronin_settings file remotely
-command = f"""echo -e '# if you place this file at `/etc/puppet/ronin_settings`\n# the `run-puppet.sh` script will use the values here.\n\n# puppet overrides\nPUPPET_REPO={puppet_repo}\nPUPPET_BRANCH={puppet_branch}\nPUPPET_MAIL={puppet_mail}\n\n# taskcluster overrides\n# WORKER_TYPE_OVERRIDE=gecko-t-linux-talos-1804-staging' | sudo tee /etc/puppet/ronin_settings"""
+command = f"""echo -e '# if you place this file at `/etc/puppet/ronin_settings`\n# the `run-puppet.sh` script will use the values here.\n\n# puppet overrides\nPUPPET_REPO=\"{puppet_repo}\"\nPUPPET_BRANCH=\"{puppet_branch}\"\nPUPPET_MAIL=\"{puppet_mail}\"\n\n# taskcluster overrides\n# WORKER_TYPE_OVERRIDE=gecko-t-linux-talos-1804-staging' | sudo tee /etc/puppet/ronin_settings"""
 
 # SSH to the remote machine and execute the command
 try:
