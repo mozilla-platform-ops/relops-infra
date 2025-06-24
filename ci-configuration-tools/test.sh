f63966ddba93c6f39cfc601815f87e1be6fa715d
Andrew Erickson <aerickson@mozilla.com>
2025-06-24 11:28:53 -0500

    test: make faster, less complete

--- ci-configuration-tools/test.sh
+++ ci-configuration-tools/test.sh
@@ -7,4 +7,9 @@ export TASKCLUSTER_ROOT_URL="https://firefox-ci-tc.services.mozilla.com/"
 # venv-based
 #
 . ./venv/bin/activate
-ci-admin check --environment firefoxci
+
+# slower, more complete
+# ci-admin check --environment firefoxci
+
+# faster
+ci-admin check --environment firefoxci --resources worker_pools
