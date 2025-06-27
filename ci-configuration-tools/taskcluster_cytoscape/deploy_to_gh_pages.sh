f69dbf9b14a3fda6314a383aef3468c5ac4f6c1a
Andrew Erickson <aerickson@mozilla.com>
2025-06-27 16:52:32 -0500

    tweak deploy script

--- ci-configuration-tools/taskcluster_cytoscape/deploy_to_gh_pages.sh
+++ ci-configuration-tools/taskcluster_cytoscape/deploy_to_gh_pages.sh
@@ -11,11 +11,14 @@ GH_PAGES_REPO_PATH="$HOME/git/gh_pages_test/taskcluster_cytoscape_viewer/"
 cp worker_pools_images.cyto.json $GH_PAGES_REPO_PATH
 
 # copy worker_pools_viewer_focusmode_multiselect.html to GH_PAGES_REPO_PATH
-cp cytoscape_viewer/worker_pools_viewer_focusmode_multiselect.html $GH_PAGES_REPO_PATH
+# cp cytoscape_viewer/worker_pools_viewer_focusmode_multiselect.html $GH_PAGES_REPO_PATH
+# copy to easier path
+cp cytoscape_viewer/worker_pools_viewer_focusmode_multiselect.html $GH_PAGES_REPO_PATH/viewer.html
+
 
 cd $GH_PAGES_REPO_PATH
 # commit changes
-git add worker_pools_images.cyto.json worker_pools_viewer_focusmode_multiselect.html
+git add worker_pools_images.cyto.json viewer.html
 # check if there are changes before committing
 if [ -z "$(git status --porcelain)" ]; then
   echo "No changes to commit."
