e6c5b21c545dcec7f31bacb5a16b1ce3531eb171
Andrew Erickson <aerickson@mozilla.com>
2025-06-27 16:32:50 -0500

    add deploy to gh pages script

---
+++ ci-configuration-tools/taskcluster_cytoscape/deploy_to_gh_pages.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# this only handles updates... initial deployment needs to do symlink work
+
+GH_PAGES_REPO_PATH="$HOME/git/gh_pages_test/taskcluster_cytoscape_viewer/"
+
+# copy worker_pools_images.cyto.json to GH_PAGES_REPO_PATH
+cp worker_pools_images.cyto.json $GH_PAGES_REPO_PATH
+
+# copy worker_pools_viewer_focusmode_multiselect.html to GH_PAGES_REPO_PATH
+cp cytoscape_viewer/worker_pools_viewer_focusmode_multiselect.html $GH_PAGES_REPO_PATH
+
+cd $GH_PAGES_REPO_PATH
+# commit changes
+git add worker_pools_images.cyto.json worker_pools_viewer_focusmode_multiselect.html
+# check if there are changes before committing
+if [ -z "$(git status --porcelain)" ]; then
+  echo "No changes to commit."
+  exit 0
+fi
+git commit -m "Update worker pools viewer files"
+# push changes
+git push origin main
