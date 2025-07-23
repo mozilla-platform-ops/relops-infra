151fca6d9e9c0fa09fa81f586b52a85b80446faa
Andrew Erickson <aerickson@mozilla.com>
2025-07-23 16:32:58 -0700

    add testing tools

---
+++ ubuntu-autoinstall/test_autostart_file.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# filepath: /Users/aerickson/git/relops-infra3/ubuntu-autoinstall/run.sh
+# Run the image interactively, mounting the current directory as /etc/autoinstall for convenience
+# docker run --rm -it -v "$PWD":/etc/autoinstall ubuntu-autoinstall:latest /bin/bash
+
+# TODO: instead of an interactive shell, run the /tools/subiquity/scripts/validate-autoinstall-user-data.py script on the input file
+# to validate the autoinstall user data.
+docker run --rm -v "$PWD":/etc/autoinstall ubuntu-autoinstall:latest /tools/subiquity/scripts/validate-autoinstall-user-data.py /etc/autoinstall/$@
